29871d7e1be3446be3981eebba97fc335892983f
Ariel Shaqed (Scolnicov) <ariels@binaris.com>
2018-04-29 18:34:39 +0300

    CR fixes: pipefail, stricter logic.

--- scipy/build.sh
+++ scipy/build.sh
@@ -3,6 +3,8 @@
 # Builds bolt-compatible packages from Python requirements, and copies
 # them back out to CWD.
 
+set -euo pipefail
+
 dir=`dirname $0`
 
 if [ -w /var/run/docker.sock ]; then
@@ -13,14 +15,13 @@ fi
 
 mkdir -p .binaris/.pycache dist
 
-$DOCKER build --tag=pipper $dir # Keep going even on failure!
+$DOCKER build --tag=pipper $dir
 if [ -f ./requirements.txt ]; then
   volume="-v $PWD/requirements.txt:/function/requirements.txt"
 else
   volume=''
 fi
-$DOCKER run $volume pipper:latest
-build_status=$?
-$DOCKER run pipper:latest bash -c 'tar -C /cache -cz .' | tar -x --keep-newer -C .binaris/.pycache/
-$DOCKER run pipper:latest bash -c 'tar -C /function -cz .' | tar -x --keep-newer -C dist/
+$DOCKER run --rm $volume pipper:latest && build_status=0 || build_status=$? && true
+$DOCKER run --rm pipper:latest bash -c 'tar -C /cache -cz .' | tar -x --keep-newer -C .binaris/.pycache/ || true
+$DOCKER run --rm pipper:latest bash -c 'tar -C /function -cz .' | tar -x --keep-newer -C dist/ || true
 exit $build_status
